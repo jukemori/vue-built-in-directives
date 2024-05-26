7924a6e5b0863f247c5325f2bdbb31efcdbba6d5
Jun Ukemori <jukemori@gmail.com>
2024-05-26 14:10:25 +0900

    add v-bind feature

---
+++ src/components/Vbind.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img 
+      v-bind:src="imageSrc" 
+      v-bind:width="imageWidth" 
+      v-bind:height="imageHeight" 
+      alt="Apple"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1920px-Red_Apple.jpg',
+      imageWidth: '300px', 
+      imageHeight: '300px' 
+    }
+  }
+}
+</script>
