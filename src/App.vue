5a483ff0109e68001b1ee3824205dbd6b9290631
Jun Ukemori <jukemori@gmail.com>
2024-05-26 16:37:15 +0900

    add v-if code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
     <!-- <Vbind /> -->
-    <Vmodel />
+    <!-- <Vmodel /> -->
+    <Vif />
   </div>
 </template>
 
 <script>
 import Vbind from "./components/Vbind.vue";
 import Vmodel from "./components/Vmodel.vue";
+import Vif from "./components/Vif.vue";
 
 export default {
   components: {
     Vbind,
-    Vmodel
+    Vmodel,
+    Vif,
   },
-  // data() {
-  //   return {
-  //     imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1920px-Red_Apple.jpg'
-  //   }
-  // }
 }
 </script>
---
+++ src/components/Vif.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p v-if="type === 'A'">Type A</p>
+    <p v-else-if="type === 'B'">Type B</p>
+    <p v-else>Other Type</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: 'A'
+    }
+  }
+}
+</script>
